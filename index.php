4b92c5cd625eae165c0e654ccda8720abf6577fc
ChrisWayFor <christopher.farcoz@gmail.com>
2024-04-02 13:03:09 +0200

    resolv2

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php 
 const BR = '<br />';
-
 function writeSecretSentence($paramOne, $paramTwo) {
     $read = "$paramOne s'incline face à $paramTwo";
     return $read;
@@ -8,8 +7,7 @@ function writeSecretSentence($paramOne, $paramTwo) {
 $numberOne = writeSecretSentence("La grenouille sauteuse", "la lapine garou du tibet");
 $numberTwo = writeSecretSentence("Le sanglier maraudeur", "L'ours renard du japon");
 $NumberThree = writeSecretSentence("LAISSE MOI PASSER !", "Alohomora ? Non ? toujours pas ?");
-
 echo $numberOne . BR;
 echo $numberTwo . BR;
 echo $NumberThree . BR;
-
+?>
